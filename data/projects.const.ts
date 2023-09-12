54e6ea8d71b7b9033435a0505357aea655ac2ed8
Kylie-Lang1 <kylielang@pursuit.org>
2023-09-12 11:23:37 -0400

    create data file for project info

---
+++ data/projects.const.ts
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        id: 1,
+        title: 'Kick.It',
+        description: 'A five-week capstone project that allows users to create an account, search for other users, chat with friends, browse hosted events by category and host their own events.',
+        technologies: [
+            'React', 'Tailwind Css', 'Google Maps API', 'Firebase', 'Socket.io'
+        ],
+        github: ['https://github.com/Kylie-Lang1/kick-it-frontend', 'https://github.com/Kylie-Lang1/kick-it-backend' ],
+    },
+    {
+        id: 2,
+        title: 'FitMe',
+        description: 'An individual project that creates a virtual closet for users to add articles of clothing and compose different outfits',
+        technologies: [
+            'React', 'Express', 'PostgreSQL', 'Node.js'
+        ],
+        github: ['https://github.com/Kylie-Lang1/FitMe-front-end-react', 'https://github.com/Kylie-Lang1/FitMe-back-end-express-sql' ],
+    }
+]
