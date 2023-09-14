b3ac174b23c292c0ebc28709bceb7e92dbfd4a8c
Kylie-Lang1 <kylielang@pursuit.org>
2023-09-14 18:11:33 -0500

    landing page animations

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import NextLink from "next/link";
 import React, { useState, useEffect } from 'react';
+import Image from "next/image";
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
@@ -11,28 +12,51 @@ import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import SplitType from 'split-type'
 import { gsap } from "gsap"
+import * as Logos from '../assets/home/importExport'
+import './home.css'
 
 export default function Home() {
 	const [init, setInit] = useState<HTMLElement[] | null>(null);
 	// Similar to componentDidMount and componentDidUpdate:
 	useEffect(() => {
 		if (!init) {
-			const text = SplitType.create('#target')
+			// split name, title and intro on landing page
+			const name = SplitType.create('#name', {types: 'chars'})
+			const title = SplitType.create('#title', {types: 'lines'})
+			const intro = SplitType.create('#into', {types: 'chars'})
 
-			setInit(text.chars)
+			console.log(name.chars, title.lines, intro.chars)
+
+			setInit([name.chars, title.lines, intro.chars])
 			
-			if (text.chars) {
-				for (let i = 0; i < text.chars.length; i++) {
-					text.chars[i].classList.add('translate-y-full')
-				}
-			}
+			// animation for each character of #name
+			// if (name.chars) {
+			// 	for (let i = 0; i < name.chars.length; i++) {
+			// 		name.chars[i].classList.add('translate-y-full')
+			// 	}
+			// }
 			
 			gsap.to('.char', {
 				y:0,
 				stagger: 0.05,
-				delay:0.02,
+				delay:0.2,
 				duratioin: 0.5
 			})
+
+			// animation for each line of #title
+			// if(title.lines){
+			// 	for (let i=0; i < title.lines.length; i++){
+			// 		title.lines[i].classList.add('translate-y-[115]')
+			// 	}
+			// }
+
+			gsap.to('.line', {
+				y:0,
+				delay: 1
+			})
+
+			// animation for each char of #intro
+			
 
 			// SplitType.revert('#target')
 		}
@@ -41,18 +65,71 @@ export default function Home() {
 
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 id='target' className={`${title({ color: "violet" })} target text-violet-600`}>Kylie Lang.</h1>
-				<br />
-				<h1 className={`${title()} `}>
+			<div className="inline-block max-w-lg text-center justify-center my-20">
+				<h1 id='name' className={`${title({ color: "violet" })} text-violet-600 mb-16 z-40`}>
+					Kylie Lang.
+				</h1>
+				<br/>
+				<h1 id='title' className={`${title()}`}>
 					Full Stack Software Developer.
 				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
+				<h2 id='intro' className={`${subtitle({ class: "mt-4" })} mt-[50px]`}>
 					Welcome to my portfolio.
 				</h2>
 			</div>
-			<div>
-
+			<div className="w-full inline-flex flex-nowrap h-32">
+				<ul className="h-32 flex items-center justify-center md:justify-start animate-infinite-scroll overflow-hidden">
+					<li>
+						<Image src={Logos.express_Logo} alt='express-logo'className="bg-transparent"/>
+					</li>
+					<li>
+						<Image src={Logos.html_Js_Css_Logo} alt='html-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.js_Logo} alt='js-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.next_Logo} alt='next-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.node_Logo} alt='node-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.postresql_Logo} alt='postgresql-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.react_Logo} alt='react-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.tailwind_Logo} alt='tailwind-logo'/>
+					</li>
+				</ul>
+				<ul className="flex items-center justify-center md:justify-start h-32 animate-infinite-scroll overflow-hidden">
+					<li>
+						<Image src={Logos.express_Logo} alt='express-logo'className="bg-transparent"/>
+					</li>
+					<li>
+						<Image src={Logos.html_Js_Css_Logo} alt='html-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.js_Logo} alt='js-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.next_Logo} alt='next-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.node_Logo} alt='node-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.postresql_Logo} alt='postgresql-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.react_Logo} alt='react-logo'/>
+					</li>
+					<li>
+						<Image src={Logos.tailwind_Logo} alt='tailwind-logo'/>
+					</li>
+				</ul>
 			</div>
 		</section>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,17 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 20s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }                    
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
