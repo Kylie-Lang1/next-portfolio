0d87466bd6b43cce1d7b23cb78bb625d36c610c7
Kylie-Lang1 <kylielang@pursuit.org>
2023-09-14 18:12:09 -0500

    adding logo imgs and organizing assets

---
+++ assets/home/importExport.js
@@ -0,0 +1,15 @@
+export const express_Logo = require('./express-logo.jpeg')
+
+export const html_Js_Css_Logo = require('./html5-js-css3-logo.png')
+
+export const js_Logo = require('./js-logo.jpeg')
+
+export const next_Logo = require('./next-logo.png')
+
+export const node_Logo = require('./node-js-logo.jpeg')
+
+export const postresql_Logo = require('./postrgresql-logo.png')
+
+export const react_Logo = require('./react-logo.png')
+
+export const tailwind_Logo = require('./tailwindcss-logo.jpeg')
